ec98eac00ae4d759812ffffd1ce7289e8a786575
Kornel Takacs <tkornel.takacs2000@gmail.com>
2023-08-31 11:26:22 +0200

    Fix the variable name

--- Stock Manager Simulator Backend/Stock Manager Simulator Backend/Dtos/RegisterDto.cs
+++ Stock Manager Simulator Backend/Stock Manager Simulator Backend/Dtos/RegisterDto.cs
@@ -7,7 +7,7 @@
         public string ConfirmPassword { get; set; } = null!;
         public string Firstname { get; set; } = null!;
         public string Lastname { get; set; } = null!;
-        public DateTime BirtOfDate { get; set; }
+        public DateTime BirthOfDate { get; set; }
         public string Email { get; set; } = null!;
         public string Gender { get; set; } = null!;
     }
